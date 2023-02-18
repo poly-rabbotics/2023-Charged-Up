b5ab63a3f60cc8a12d49b757e7e67daba5f111da
AEElpusan <92772579+AEElpusan@users.noreply.github.com>
2023-02-18 15:26:21 -0800

    Rack code testable

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Subsystems.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -78,7 +79,9 @@ public class Robot extends TimedRobot {
 
     /** This function is called periodically during operator control. */
     @Override
-    public void teleopPeriodic() {}
+    public void teleopPeriodic() {
+        Intake.runRack();
+    }
 
     /** This function is called once when the robot is disabled. */
     @Override
