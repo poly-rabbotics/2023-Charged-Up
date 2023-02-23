56930c4ad5b03cd04e6d51107dd8cbef1acc34f1
JustBeChill <kmalling16@gmail.com>
2023-02-22 18:00:53 -0800

    Switched auto balance to a toggle

--- src/main/java/frc/robot/systems/Drive.java
+++ src/main/java/frc/robot/systems/Drive.java
@@ -24,51 +24,57 @@ public class Drive {
     private static final double DRIVE_DEADZONE = 0.3;
     private static final int PANCAKE_FORWARD_CHANNEL_ID = 0; //PLACEHOLDERS, REPLACE LATER
     private static final int PANCAKE_REVERSE_CHANNEL_ID = 1;
-
+    
     private static Drive instance = new Drive();
-
+    
     private DriveUnit rightUnit;
     private DriveUnit leftUnit;
     
     private SparkMaxPIDController rightController;
     private SparkMaxPIDController leftController;
     private DoubleSolenoid gearPancake;
-
+    
     private static final double P = 0.0000001;
     private static final double I = 0;
     private static final double D = 0;
     private double velocitySetPoint = 0;
-
+    
     private boolean highGearActive;
-
+    private boolean isBalancing;
+    
     private Drive() {
         rightUnit = new DriveUnit(RIGHT_LEADER_CAN_ID, RIGHT_FOLLOWER_CAN_ID);
         leftUnit = new DriveUnit(LEFT_LEADER_CAN_ID, LEFT_FOLLOWER_CAN_ID);
-
+        
         gearPancake = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PANCAKE_FORWARD_CHANNEL_ID, PANCAKE_REVERSE_CHANNEL_ID);
-
+        
         rightUnit.settupPID(P, I, D);
         leftUnit.settupPID(P, I, D);
-
+        
         rightController = rightUnit.getController();
         leftController = leftUnit.getController();
     }
-
+    
+    public static void init() {
+        instance.isBalancing = false;
+        instance.highGearActive = false;
+    }
+    
     /**
-     * Method to run the drive system during teleop
-     */
-    public static void run(double joystickX, double joystickY, boolean runAutoBalance, int dPadDirection, boolean runRockMode, boolean toggleGearShift) {
+    * Method to run the drive system during teleop
+    */
+    public static void run(double joystickX, double joystickY, boolean toggleAutoBalance, int dPadDirection, boolean runRockMode, boolean toggleGearShift) {
         
-        if(runAutoBalance) { //if auto balance button is held
-            autoBalance();
+        if(toggleAutoBalance) { //if auto balance button is held
+            autoBalance(toggleAutoBalance);
         } else if(runRockMode) { //if rock mode button ins held
             rockMode(joystickX, joystickY);
         } else { //if no special mode is active
             //runs if dpad is pressed
             lowSensitivityMode(dPadDirection);
-
+            
             gearShift(toggleGearShift);
-
+            
             // rotates joystick axes by 45 degrees & sets units to left and right axes
             if(Math.abs(joystickX) > DRIVE_DEADZONE) {
                 instance.rightUnit.set(joystickX + joystickY);
@@ -76,28 +82,38 @@ public class Drive {
             }
         }
     }
-
+    
     /**
-     * Uses PID control to automatically level on charging station, 
-     * Detects whether to go forwards or backwards by checking the pitch of the robot
-     * and the rate of change of the pitch
-     */
-    private static void autoBalance() {
-        if (Pigeon.getRelativePitch() <= -TILT_DEADZONE && Pigeon.getChangePerSecond().pitch > 0.1) { //if robot is tilted forwards
-            instance.velocitySetPoint = -BALANCING_RPM;
-        } else if (Pigeon.getRelativePitch() >= TILT_DEADZONE && Pigeon.getChangePerSecond().pitch > 0.1) { //if robot is tilted backwards
-            instance.velocitySetPoint = BALANCING_RPM;
-        } else { //if robot is not tilted
-            instance.velocitySetPoint = 0;
+    * Uses PID control to automatically level on charging station, 
+    * Detects whether to go forwards or backwards by checking the pitch of the robot
+    * and the rate of change of the pitch
+    */
+    private static void autoBalance(boolean toggleAutoBalance) {
+        if(toggleAutoBalance) {
+            if(instance.isBalancing) {
+                instance.isBalancing = false;
+            } else {
+                instance.isBalancing = true;
+            }
         }
-
-        instance.rightController.setReference(instance.velocitySetPoint, ControlType.kVelocity);
-        instance.leftController.setReference(instance.velocitySetPoint, ControlType.kVelocity);
-
-        instance.rightUnit.setController(instance.rightController);
-        instance.leftUnit.setController(instance.leftController);
+        
+        if(instance.isBalancing) {
+            if (Pigeon.getRelativePitch() <= -TILT_DEADZONE && Pigeon.getChangePerSecond().pitch > 0.1) { //if robot is tilted forwards
+                instance.velocitySetPoint = -BALANCING_RPM;
+            } else if (Pigeon.getRelativePitch() >= TILT_DEADZONE && Pigeon.getChangePerSecond().pitch > 0.1) { //if robot is tilted backwards
+                instance.velocitySetPoint = BALANCING_RPM;
+            } else { //if robot is not tilted
+                instance.velocitySetPoint = 0;
+            }
+            
+            instance.rightController.setReference(instance.velocitySetPoint, ControlType.kVelocity);
+            instance.leftController.setReference(instance.velocitySetPoint, ControlType.kVelocity);
+            
+            instance.rightUnit.setController(instance.rightController);
+            instance.leftUnit.setController(instance.leftController);
+        }
     }
-
+    
     private static void lowSensitivityMode(int dPadDirection) {
         if(dPadDirection == 0) { //if dPad is pressed up, move forward
             instance.rightUnit.set(0.5);
@@ -116,7 +132,7 @@ public class Drive {
             instance.leftUnit.set(0);
         }
     }
-
+    
     private static void rockMode(double joystickX, double joystickY) {
         if(Math.abs(joystickX) > DRIVE_DEADZONE) {
             instance.rightController.setReference((joystickX + joystickY) * ROCK_MODE_RPM, ControlType.kVelocity);
@@ -126,7 +142,7 @@ public class Drive {
             instance.leftController.setReference(0, ControlType.kVelocity);
         }
     }
-
+    
     private static void gearShift(boolean toggleGearShift) {
         if(toggleGearShift) {
             if(instance.highGearActive) {
