cd6eb04c60cf71e2b417ca4a90cfda8c9c4283ae
rohancreddy9 <rohancreddy@gmail.com>
2023-02-23 09:01:48 -0800

    PLG Motor Test Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -80,7 +80,8 @@ public class Robot extends TimedRobot {
     /** This function is called periodically during operator control. */
     @Override
     public void teleopPeriodic() {
-        Intake.runRack();
+        //Intake.runRack();
+        Test.run();
     }
 
     /** This function is called once when the robot is disabled. */
--- src/main/java/frc/robot/Subsystems/Test.java
+++ src/main/java/frc/robot/Subsystems/Test.java
@@ -1,23 +1,23 @@
 package frc.robot.Subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 
 public class Test {
-    TalonSRX motor;
+    PWMTalonSRX motor;
     Joystick stick;   
-
-    public Test() {
-        motor = new TalonSRX(0);
+    private static Test instance = new Test();
+    private Test() {
+        motor = new PWMTalonSRX(0);
         stick = new Joystick(0);
 
     }
 
-    public void run() {
-        double speed = stick.getRawAxis(1);
-        motor.set(ControlMode.PercentOutput, speed);
+    public static void run() {
+        double speed = instance.stick.getRawAxis(1);
+        instance.motor.set(speed);
     }
 
 }
