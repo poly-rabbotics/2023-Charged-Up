f9182f730592fbd7f1dd48f6236586d619736bcd
JustBeChill <kmalling16@gmail.com>
2023-03-23 16:29:10 -0700

    Auto modes class written

---
+++ src/main/java/frc/robot/AutoModes.java
@@ -0,0 +1,294 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.systems.Intake.SolenoidState;
+import frc.robot.systems.ElevFourbar.Setpoint;
+import frc.robot.systems.*;
+import frc.robot.patterns.*;
+
+public class AutoModes {
+    //Control panel used for calculating auto mode
+    private static Joystick controlPanel = (Joystick)Controls.getControllerByPort(2);
+    
+    private static Timer timer = new Timer();
+    private static Timer secondaryTimer = new Timer();
+    
+    private static int autoMode;
+    private static int autoStage;
+    private static double startTimeBalance = -1.0;
+    
+    public static void init() {
+        //Calculate auto mode
+        autoMode = 0;
+        
+        if(controlPanel.getRawButton(12)) 
+        autoMode += 1;
+        if(controlPanel.getRawButton(11))
+        autoMode += 2;
+        if(controlPanel.getRawButton(10))
+        autoMode += 4;
+        
+        ElevFourbar.autonomousInit();
+        Pigeon.setFeildZero();
+        LEDLights.setPatternIfNotEqual(new Breathe(new Color(0.8, 0.3, 0.0), 0.5));
+        
+        timer.reset();
+        timer.start();
+        
+        secondaryTimer.reset();
+        secondaryTimer.start();
+        autoStage = 0;
+    }
+    
+    public static void run() {
+        //Run the auto mode
+        switch (autoMode) {
+            case 0:
+            //Do nothing
+            break;
+            case 1:
+            modeOne();
+            break;
+            case 2:
+            //Do something else
+            break;
+            case 3:
+            //Do something else
+            break;
+            case 4:
+            //Do something else
+            break;
+            case 5:
+            //Do something else
+            break;
+            case 6:
+            //Do something else
+            break;
+            case 7:
+            //Do something else
+            break;
+        }
+    }
+    
+    /**
+     * Scores mid then moves out of community
+     */
+    private static void modeOne() {
+        if(autoStage == 0){
+            //Move the pivot up
+            Intake.autoPivot(SolenoidState.UP);
+            
+            //Move the elevator to the mid scoring position
+            if(ElevFourbar.autoRun(Setpoint.MID_SCORING)) {
+                //Open claw when the position has been reached
+                secondaryTimer.reset();
+                Intake.autoClaw(SolenoidState.OPEN);
+                autoStage++;
+            } 
+        } else {
+            //1 second delay to prevent closing on the cube again >:(
+            if(secondaryTimer.get() > 1) {
+                //Move to stowed setpoint
+                if(ElevFourbar.autoRun(Setpoint.STOWED)) {
+                    //Close the claw and put the pivot down
+                    Intake.autoClaw(SolenoidState.CLOSED);
+                    Intake.autoPivot(SolenoidState.DOWN);
+                }
+            }
+        }
+        
+        if (timer.get() > 10 && timer.get() < 15) {
+            SwerveDrive.run(0.0, -0.75, 0.0, -1);
+        } else {
+            SwerveDrive.run(0.0, 0.0, 0.0, -1);
+        }
+    }
+
+    /**
+     * Score only mid
+     */
+    private static void modeTwo() {
+        if(autoStage == 0){
+            //Move the pivot up
+            Intake.autoPivot(SolenoidState.UP);
+
+            //Move to mid scoring position
+            if(ElevFourbar.autoRun(Setpoint.MID_SCORING)) {
+                //Open claw when the position has been reached
+                Intake.autoClaw(SolenoidState.OPEN);
+                secondaryTimer.reset();
+                
+                autoStage++;
+            } 
+        } else {
+            //1 second delay to prevent closing on the cube again >:(
+            if(secondaryTimer.get() > 1) {
+                //Move to stowed setpoint
+                if(ElevFourbar.autoRun(Setpoint.STOWED)) {
+                    //Close the claw and put the pivot down
+                    Intake.autoClaw(SolenoidState.CLOSED);
+                    Intake.autoPivot(SolenoidState.DOWN);
+                }
+            }
+        }
+    }
+
+    /**
+     * Score mid then auto balance
+     */
+    private static void modeThree() {
+        if(autoStage == 0){
+            //Move the pivot up
+            Intake.autoPivot(SolenoidState.UP);
+
+            //Move to mid scoring position
+            if(ElevFourbar.autoRun(Setpoint.MID_SCORING)) {
+                //Open claw when the position has been reached
+                Intake.autoClaw(SolenoidState.OPEN);
+                secondaryTimer.reset();
+                
+                autoStage++;
+            } 
+        } else {
+            //1 second delay to prevent closing on the cube again >:(
+            if(secondaryTimer.get() > 1) {
+                //Move to stowed setpoint
+                if(ElevFourbar.autoRun(Setpoint.STOWED)) {
+                    //Close the claw and put the pivot down
+                    Intake.autoClaw(SolenoidState.CLOSED);
+                    Intake.autoPivot(SolenoidState.DOWN);
+                }
+            }
+        }
+
+        autoBalance(8.0);
+    }
+
+    /**
+     * Score high then move out of the community
+     */
+    private static void modeFour() {
+        if(autoStage == 0){
+            //Move the pivot up
+            Intake.autoPivot(SolenoidState.UP);
+            
+            //Move the elevator to the high scoring position
+            if(ElevFourbar.autoRun(Setpoint.HIGH_SCORING)) {
+                //Open claw when the position has been reached
+                secondaryTimer.reset();
+                Intake.autoClaw(SolenoidState.OPEN);
+                autoStage++;
+            } 
+        } else {
+            //1 second delay to prevent closing on the cube again >:(
+            if(secondaryTimer.get() > 1) {
+                //Move to stowed setpoint
+                if(ElevFourbar.autoRun(Setpoint.STOWED)) {
+                    //Close the claw and put the pivot down
+                    Intake.autoClaw(SolenoidState.CLOSED);
+                    Intake.autoPivot(SolenoidState.DOWN);
+                }
+            }
+        }
+        
+        if (timer.get() > 10 && timer.get() < 15) {
+            SwerveDrive.run(0.0, -0.75, 0.0, -1);
+        } else {
+            SwerveDrive.run(0.0, 0.0, 0.0, -1);
+        }
+    }
+
+    /**
+     * Only score high
+     */
+    private static void modeFive() {
+        if(autoStage == 0){
+            //Move the pivot up
+            Intake.autoPivot(SolenoidState.UP);
+            
+            //Move the elevator to the high scoring position
+            if(ElevFourbar.autoRun(Setpoint.HIGH_SCORING)) {
+                //Open claw when the position has been reached
+                secondaryTimer.reset();
+                Intake.autoClaw(SolenoidState.OPEN);
+                autoStage++;
+            } 
+        } else {
+            //1 second delay to prevent closing on the cube again >:(
+            if(secondaryTimer.get() > 1) {
+                //Move to stowed setpoint
+                if(ElevFourbar.autoRun(Setpoint.STOWED)) {
+                    //Close the claw and put the pivot down
+                    Intake.autoClaw(SolenoidState.CLOSED);
+                    Intake.autoPivot(SolenoidState.DOWN);
+                }
+            }
+        }
+    }
+
+    /**
+     * Score high and auto balance
+     */
+    private static void modeSix() {
+        if(autoStage == 0){
+            //Move the pivot up
+            Intake.autoPivot(SolenoidState.UP);
+            
+            //Move the elevator to the high scoring position
+            if(ElevFourbar.autoRun(Setpoint.HIGH_SCORING)) {
+                //Open claw when the position has been reached
+                secondaryTimer.reset();
+                Intake.autoClaw(SolenoidState.OPEN);
+                autoStage++;
+            } 
+        } else {
+            //1 second delay to prevent closing on the cube again >:(
+            if(secondaryTimer.get() > 1) {
+                //Move to stowed setpoint
+                if(ElevFourbar.autoRun(Setpoint.STOWED)) {
+                    //Close the claw and put the pivot down
+                    Intake.autoClaw(SolenoidState.CLOSED);
+                    Intake.autoPivot(SolenoidState.DOWN);
+                }
+            }
+        }
+
+        autoBalance(0.8);
+    }
+
+    /**
+     * Only move out of the community
+     */
+    private static void modeSeven() {
+        if (timer.get() > 10 && timer.get() < 15) {
+            SwerveDrive.run(0.0, -0.75, 0.0, -1);
+        } else {
+            SwerveDrive.run(0.0, 0.0, 0.0, -1);
+        }
+    }
+
+    private static void autoBalance(double startTime) {
+        if (timer.get() > startTime) {
+            if (startTimeBalance == -1.0) {
+                startTimeBalance = timer.get();
+            }
+
+            if (timer.get() - startTimeBalance >= 1.5) {
+                SwerveDrive.run(0.0, 0.0, 0.0, -1);
+                return;
+            }
+
+            /* if (false) {
+                startTimeBalance = -1.0;
+                if (timer.get() > 0 && timer.get() < 15) {
+                    SwerveDrive.run(0.0, -0.85, 0.0, -1);
+                } else {
+                    SwerveDrive.run(0.0, 0.0, 0.0, -1);
+                }
+            } */
+        }
+    }
+}
