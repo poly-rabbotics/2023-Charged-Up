240dfa3733fe10e597bde86e8d67cb3fb3070f76
Evan Overman <evanrileyoverman@gmail.com>
2023-04-06 23:27:15 -0700

    printing stuff

---
+++ src/main/java/frc/robot/systems/SmartPrinter.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.systems;
+
+import java.security.InvalidParameterException;
+import java.util.LinkedList;
+
+import frc.robot.SmartPrintable;
+
+public class SmartPrinter {
+    private LinkedList<SmartPrintable> printables;
+    private static final SmartPrinter instance = new SmartPrinter();
+
+    public SmartPrinter() {
+        printables = new LinkedList<SmartPrintable>();
+    }
+
+    public static void register(SmartPrintable printable) {
+        if (printable == null) {
+            throw new InvalidParameterException("Cannot pass null to SmartPrinter.register()");
+        }
+
+        instance.printables.add(printable);
+    }
+
+    public static void print() {
+        for (SmartPrintable printable : instance.printables) {
+            printable.print();
+        }
+    }
+}
